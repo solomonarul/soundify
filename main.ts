c45446b1b638c033f852813f0294790fca23c174
Solomonarul <solomonarul@tuta.io>
2025-12-12 01:22:40 +0200

    feat(repo): Add some sounds.

--- main.ts
+++ main.ts
@@ -1,134 +1,67 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+type PluginAudio = HTMLAudioElement | null;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class Soundify extends Plugin {
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	private startupAudio: PluginAudio = null;
+	private basesHoverAudio: PluginAudio = null;
+	private observer: MutationObserver | null = null;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	private getLocalPath(resource: string) : string {
+		return this.app.vault.adapter.getResourcePath(
+			`.obsidian/plugins/${this.manifest.id}/${resource}`
+		);
+	}
+
+	private createAudioElement(path: string) {
+		var result = new Audio(this.getLocalPath(path));
+		result.preload = "auto";
+		return result;
+	}
 
 	async onload() {
-		await this.loadSettings();
+		this.startupAudio = this.createAudioElement("media/startup.mp3");
+		this.basesHoverAudio = this.createAudioElement("media/basesHover.mp3");
+	
+        this.enableObserver();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.startupAudio!.currentTime = 0;
+		this.startupAudio!.play().catch(() => {});
 	}
 
 	onunload() {
-
+		if (this.observer) this.observer.disconnect();
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	private enableObserver() {
+		// Watch DOM updates to detect Bases cards
+		this.observer = new MutationObserver(() => {
+			this.attachBasesHoverListeners();
+		});
+		this.observer.observe(document.body, {
+			childList: true,
+			subtree: true
+		});
+
+		// Attach on startup
+		this.attachBasesHoverListeners();
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	private attachBasesHoverListeners() {
+		if (!this.basesHoverAudio) return;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		const cards = document.querySelectorAll(".bases-cards-item");
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		cards.forEach(card => {
+			// Prevent double-binding
+			if ((card as any)._hoverSoundBound) return;
+			(card as any)._hoverSoundBound = true;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			card.addEventListener("mouseenter", () => {
+				this.basesHoverAudio!.currentTime = 0;
+				this.basesHoverAudio!.play().catch(() => {});
+			});
+		});
 	}
 }
